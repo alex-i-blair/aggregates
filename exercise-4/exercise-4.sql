1a2fbc27ca5f1839609c1ad57a2faae464115db4
Alex Blair <bclan11@gmail.com>
2022-03-23 15:28:01 -0700

    finished through 8

--- exercise-4/exercise-4.sql
+++ exercise-4/exercise-4.sql
@@ -1,2 +1,10 @@
 -- the average film length by category
 -- include the category name and avg length
+SELECT name, AVG(length)
+FROM film
+LEFT JOIN film_category
+ON film.film_id = film_category.film_id
+LEFT JOIN category
+ON film_category.category_id = category.category_id
+GROUP BY name
+-- ORDER BY name
